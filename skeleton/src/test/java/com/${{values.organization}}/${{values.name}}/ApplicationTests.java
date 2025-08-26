1ea8c4514e2647ee1d3c266ab5a1c70433c70ee0
TASSEAROLE <bizdiallokramoko@gmail.com>
2025-08-26 09:17:18 +0100

    .

---
+++ skeleton/src/test/java/com/${{values.organization}}/${{values.name}}/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.${{ values.organization }}.${{ values.name }};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
